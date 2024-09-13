08a759f9465f4b5f196c487fdccf676ccb3b9153
Anayatul-Ahad-Shoikot <aashoikot002@gmail.com>
2024-09-14 00:41:37 +0600

    j

--- blog_show_for_landing_page_BE.php
+++ blog_show_for_landing_page_BE.php
@@ -9,15 +9,17 @@ $query = "SELECT w.post_id, w.acc_id, w.post_title, w.post_content, w.post_image
 $result = mysqli_query($con, $query);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        echo '<div class="blog-card">';
-        echo '<img src="./assets/' . $row['post_image'] . '" alt="image">';
-        echo '<div class="blog-details">';
-        echo '<h1 class="blog-title">' . $row['post_title'] . '</h1>';
-        echo '<p class="user"></i>' . $row['acc_name'] . '</p>';
-        echo '<p class="date">' . $row['published'] . '</p>';
-        echo '<a href="./default_view_blog.php?post_id=' . $row['post_id'] . '">Read</a>';
-        echo '</div>';
-        echo '</div>';
+        echo '<div class="card">
+                <div class="card-img">
+                    <img src="./assets/' . $row['post_image'] . '" alt="image">
+                </div>
+                <div class="blog-details">
+                    <h1 class="blog-title">' . $row['post_title'] . '</h1>
+                    <p class="user"></i>' . $row['acc_name'] . '</p>
+                    <p class="date">' . $row['published'] . '</p>
+                    <a href="./default_view_blog.php?post_id=' . $row['post_id'] . '">Read</a>
+                </div>
+            </div>';
     }
 } else {
     echo '<P id="nothing_found">No blog posts found.</p>';
